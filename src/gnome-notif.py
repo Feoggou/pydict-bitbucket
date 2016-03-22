bf0ca9ab0b3575e5f8586fcd7ca278343f27d1cf
Samuel Ghinet <fio_244@yahoo.com>
2016-03-23 01:03:19 +0200

    finish gnome notification - for startup.

--- src/gnome-notif.py
+++ src/gnome-notif.py
@@ -1,19 +1,84 @@
+#!/usr/bin/python3
+
 from gi.repository import Notify
 from threading import Timer
-from time import sleep
+import sys
+import random
+import os
+import json
+
+sys.path.append("/home/zenith/PycharmProjects/EDictionary")
+dict_path = "/home/zenith/PycharmProjects/EDictionary/test-data"
 
 count_times = 0
+random.seed()
 
 Notify.init("Hello world")
 
 
-def show_notification(message: str):
-    notif_obj = Notify.Notification.new("word: <word>", message, "dialog-information")
+def get_random_word(dir_path: str) -> dict:
+    items = os.listdir(dir_path)
+    items = [x for x in items if os.path.isfile(os.path.join(dir_path, x))]
+
+    index = random.randrange(0, len(items))
+    word = items[index]
+    file_name = os.path.join(dir_path, word)
+
+    with open(file_name, "r") as f:
+        dict_obj = json.load(f)
+
+    return dict_obj
+
+
+def read_definition(definition, indent: int = 0) -> str:
+    tab = "    "
+
+    if "def_subgroup" in definition.keys():
+        s = ""
+        if "category" in definition.keys():
+            s += "({}) ".format(definition["category"])
+        s = "{} {}\n".format(tab * indent, s)
+        for subdef in definition["def_subgroup"]:
+            s += read_definition(subdef, indent + 1)
+    else:
+        s = definition["def"]
+        if "example" in definition.keys():
+            s += "\n{}e.g. {}".format(tab * (indent + 2), definition["example"])
+        if "category" in definition.keys():
+            s = "({}) {}".format(definition["category"], s)
+
+        s = (tab * indent) + " " + s
+        s += "\n"
+
+    return s
+
+
+def get_random_definition(dict_obj: dict) -> (str, str):
+    def_groups = dict_obj["def_groups"]
+    index = random.randrange(0, len(def_groups))
+
+    def_group = def_groups[index]
+    word = def_group["word"]
+    ggroups = def_group["gram_groups"]
+    index = random.randrange(0, len(ggroups))
+    ggroup = ggroups[index]
+    defs = ggroup["defs"]
+    index = random.randrange(0, len(defs))
+    definition = read_definition(defs[index])
+
+    return word, definition
+
+
+def show_notification(word: str, definition: str):
+    notif_obj = Notify.Notification.new("word:   " + word, definition, "dialog-information")
     notif_obj.show()
 
 
 def timer_callback(count):
-    show_notification("definition: <def>\n\te.g. <example>")
+    dict_obj = get_random_word(dict_path)
+    word, definition = get_random_definition(dict_obj)
+
+    show_notification(word, definition)
 
     count += 1
     if count < 3:
@@ -23,3 +88,5 @@ def timer_callback(count):
 
 timer = Timer(2, timer_callback, [count_times])
 timer.start()
+
+
