ea6cbdc91add4afaf5ec52640772406a19d779fc
Samuel Ghinet <fio_244@yahoo.com>
2016-03-24 22:16:08 +0200

    add test word: bellow

--- tests/test_create_keys.py
+++ tests/test_create_keys.py
@@ -3,6 +3,7 @@
 import http.client
 from lxml import etree
 from src.etree_printer import *
+import sys
 
 
 def get_html_for_word(word):
@@ -11,12 +12,12 @@ def get_html_for_word(word):
         hostname = "www.collinsdictionary.com"
         conn = http.client.HTTPConnection(hostname)
         conn.request("GET", "/dictionary/american/" + word)
-        reason = conn.getresponse()
-        loc = reason.getheader('location')
-        print("loc = ", loc)
-        print("reason=", reason)
-        data = reason.read()
-        print("data='{}'".format(data))
+        response = conn.getresponse()
+        loc = response.getheader('location')
+        if loc is not None:
+            print("loc = ", loc)
+
+        data = response.read()
         text = data.decode()
     else:
         f = open("old_do_defs.htm")
@@ -45,20 +46,23 @@ def get_syn_for_word(word):
     return text
 
 
-# text = get_html_for_word("adjudicator")
+add_new_word_to_test = True
+word_name = "bellow"
 
-# text = get_related_for_word("do")
-# text = get_syn_for_word("do")
-text = get_def_new("do")
+if add_new_word_to_test is True:
+    text = get_html_for_word(word_name)
+    with open(word_name + "_defs.html", "w") as f:
+        f.write(text)
 
-# with open("perform_defs.html", "w") as f:
-#     f.write(text)
+    root = etree.HTML(text)
 
-root = etree.HTML(text)
-
-# temp = sys.stdout
-# sys.stdout = open("perform_defs.keys.txt", "w")
-print_children(root, padding=0)
-# sys.stdout = temp
+    temp = sys.stdout
+    sys.stdout = open(word_name + "_defs.keys.txt", "w")
+    print_children(root, padding=0)
+    sys.stdout = temp
+else:
+    # text = get_related_for_word("do")
+    # text = get_syn_for_word("do")
+    text = get_def_new("do")
 
 # traverse_children(root, None)
