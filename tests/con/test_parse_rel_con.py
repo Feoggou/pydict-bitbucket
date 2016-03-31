6a9b26aef08496d6a83af487c87a113defdd9a71
Samuel Ghinet <fio_244@yahoo.com>
2016-03-31 23:38:07 +0300

    add word con -- add initial files

---
+++ tests/con/test_parse_rel_con.py
@@ -0,0 +1,43 @@
+import unittest
+from src.rel_parser import RelatedParser
+from lxml import etree
+
+
+class TestRelatedParser(unittest.TestCase):
+    root = None
+
+    @classmethod
+    def setUpClass(cls):
+        f = open("do_related.html")
+        text = f.read()
+        TestRelatedParser.root = etree.HTML(text)
+
+    def setUp(self):
+        self.parser = RelatedParser(TestRelatedParser.root)
+
+    def test_have_item_columns(self):
+        columns_etree = self.parser.get_def_main()
+        assert isinstance(columns_etree, etree._Element)
+
+        key = columns_etree.keys()[0]
+        value = columns_etree.get(key)
+
+        self.assertEqual((key, value), ("class", "columns"))
+
+    def test_get_all_related_words(self):
+        items = self.parser.get_all_related_words()
+
+        self.assertEqual(items, [
+            "do by", "do up", "do down", "do time", "make do", "do-gooder", "do penance", "do-or-die", "do honor to",
+            "do up right", "whoop-de-do", "do credit to", "do the honors", "do oneself well", "do-it-yourself",
+            "do one's (or its) business", "have to do with", "Mato Grosso do Sul", "do in", "to-do", "do gree",
+            "do with", "do-si-do", "derring-do", "do without", "how-do-you-do", "do to death", "tae kwon do",
+            "How do you do?", "do justice to", "do the trick", "do someone dirt", "do a number on", "do oneself justice",
+            "do one's damnedest (or damndest)", "Rio Grande do Norte", "do it", "can-do", "do over", "do-rag",
+            "do tell!", "do a deal", "do-nothing", "do duty for", "do up brown", "well-to-do", "do away with",
+            "do one's bit", "ne'er-do-well", "do wonders for", "do business with", "do oneself proud",
+            "do the bidding of", "Rio Grande do Sul"])
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/con/test_word_syn_con_to_json.py
@@ -0,0 +1,125 @@
+import unittest
+from src.syn_groups import *
+from src.syn_parser import SynParser
+from lxml import etree
+
+
+class HtmlToJsonSynTest(unittest.TestCase):
+    def setUp(self):
+        f = open("do_syn.html")
+        self.word_name = "do"
+        self.html_content = f.read()
+        self.maxDiff = None
+
+    def test_create_html_to_json_obj(self):
+        obj = HtmlToJsonSynonyms(self.word_name, self.html_content)
+        self.assertIsNotNone(obj)
+
+    def test_return_all(self):
+        obj = HtmlToJsonSynonyms(self.word_name, self.html_content)
+
+        json_obj = obj.translate()
+        self.assertEqual(json_obj, [
+            {"word": "do", "gram_groups": [
+                {"gram_group": {
+                    "value": "verb",
+                    "synonyms": [
+                        {"line": ["perform", "accomplish", "achieve", "carry out", "complete", "execute"]},
+                        {"line": ["be adequate", "be sufficient", "cut the mustard", "pass muster", "satisfy", "suffice"]},
+                        {"line": ["get ready", "arrange", "fix", "look after", "prepare", "see to"]},
+                        {"line": ["solve", "decipher", "decode", "figure out", "puzzle out", "resolve", "work out"]},
+                        {"line": ["cause", "bring about", "create", "effect", "produce"]}
+                    ]
+                }},
+                {"gram_group": {
+                    "value": "noun",
+                    "synonyms": [
+                        {"category": "informal mainly British New Zealand",
+                         "line": [
+                             "event", "affair", "function", "gathering", "occasion", "party"]
+                         },
+                    ]
+                }}
+            ]}
+        ])
+
+    def test_first_syngroup_returns_2_gram_groups(self):
+        root = etree.HTML(self.html_content)
+        dict_parser = SynParser(root, self.word_name)
+        def_group = dict_parser.get_all_def_groups()[0]
+
+        group = SynGroup(dict_parser, def_group)
+        group.build()
+        result = group.translate()
+        self.assertEqual({"word": "do", "gram_groups": [
+            {"gram_group": {
+                "value": "verb",
+                "synonyms": [
+                    {"line": ["perform", "accomplish", "achieve", "carry out", "complete", "execute"]},
+                    {"line": ["be adequate", "be sufficient", "cut the mustard", "pass muster", "satisfy", "suffice"]},
+                    {"line": ["get ready", "arrange", "fix", "look after", "prepare", "see to"]},
+                    {"line": ["solve", "decipher", "decode", "figure out", "puzzle out", "resolve", "work out"]},
+                    {"line": ["cause", "bring about", "create", "effect", "produce"]}
+                ]
+            }},
+            {"gram_group": {
+                "value": "noun",
+                "synonyms": [
+                    {"category": "informal mainly British New Zealand",
+                     "line": ["event", "affair", "function", "gathering", "occasion", "party"]},
+                ]
+            }}
+        ]}, result)
+
+    def test_first_gram_group_returns_empty_synonyms(self):
+        root = etree.HTML(self.html_content)
+        dict_parser = SynParser(root, self.word_name)
+        def_group = dict_parser.get_all_def_groups()[0]
+        gram_group = dict_parser.get_all_grammar_groups(def_group)[0]
+
+        group = SynGramGroup(dict_parser, gram_group)
+        group.build()
+        result = group.translate()
+        self.assertEqual({"gram_group": {
+            "value": "verb",
+            "synonyms": [
+                {"line": ["perform", "accomplish", "achieve", "carry out", "complete", "execute"]},
+                {"line": ["be adequate", "be sufficient", "cut the mustard", "pass muster", "satisfy", "suffice"]},
+                {"line": ["get ready", "arrange", "fix", "look after", "prepare", "see to"]},
+                {"line": ["solve", "decipher", "decode", "figure out", "puzzle out", "resolve", "work out"]},
+                {"line": ["cause", "bring about", "create", "effect", "produce"]}
+            ]
+        }}, result)
+
+    def test_1st_syn_group_list1_returns_items(self):
+        root = etree.HTML(self.html_content)
+        dict_parser = SynParser(root, self.word_name)
+        def_group = dict_parser.get_all_def_groups()[0]
+        gram_group = dict_parser.get_all_grammar_groups(def_group)[0]
+        sslist = dict_parser.get_senselist(gram_group)
+        sslitem = dict_parser.get_all_senselist_items(sslist)[0]
+
+        group = SynListGroup(dict_parser, sslitem)
+        group.build()
+        result = group.translate()
+        self.assertEqual({"line": ["perform", "accomplish", "achieve", "carry out", "complete", "execute"]}, result)
+
+    def test_1st_syn_group_returns_5_filled_lists(self):
+        root = etree.HTML(self.html_content)
+        dict_parser = SynParser(root, self.word_name)
+        def_group = dict_parser.get_all_def_groups()[0]
+        gram_group = dict_parser.get_all_grammar_groups(def_group)[0]
+
+        group = SynSenseListGroup(dict_parser, gram_group)
+        group.build()
+        result = group.translate()
+        self.assertEqual([
+            {"line": ["perform", "accomplish", "achieve", "carry out", "complete", "execute"]},
+            {"line": ["be adequate", "be sufficient", "cut the mustard", "pass muster", "satisfy", "suffice"]},
+            {"line": ["get ready", "arrange", "fix", "look after", "prepare", "see to"]},
+            {"line": ["solve", "decipher", "decode", "figure out", "puzzle out", "resolve", "work out"]},
+            {"line": ["cause", "bring about", "create", "effect", "produce"]}
+        ], result)
+
+if __name__ == '__main__':
+    unittest.main()
