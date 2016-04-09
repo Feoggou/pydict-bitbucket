1be15f01b7b17d209531dc196e6580a83d53c184
Samuel Ghinet <fio_244@yahoo.com>
2016-04-10 00:19:42 +0300

    update script.sh

--- src/script.sh
+++ src/script.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-dir_path=/home/zenith/Dropbox/Docs/DICTIONARY
+# dir_path=/home/zenith/Dropbox/Docs/DICTIONARY
+dir_path=/home/zenith/dictionary/words
+
 # for x in `ls $dir_path`; do ./py-dict.py $dir_path $x; done
 for x in `ls $dir_path`; do
-  if ! [ $(expr match "$x" '.*\(\.json\)') ]; then
-    ./py-dict.py $dir_path $x
-    if [[ $? != 0 ]]; then echo "$x" >> fails.txt; fi
-  fi
+#   if ! [ $(expr match "$x" '.*\(\.json\)') ]; then
+    # ./py-dict.py $dir_path $x
+    ./py-dict.py $x
+    if [[ $? != 0 ]]; then echo "$x" >> fails_new2.txt; fi
+#   fi
 done
 
 
