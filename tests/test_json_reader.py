be37457aa55cb8f3ca916e587fac104a468b6441
Samuel Ghinet <fio_244@yahoo.com>
2016-04-14 00:41:19 +0300

    small refactor

--- tests/test_json_reader.py
+++ tests/test_json_reader.py
@@ -8,8 +8,8 @@ from src.json_reader import JsonReader
 
 
 class TestJsonReader(unittest.TestCase):
-    word_exp_print = None
-    word_exp_json = None
+    content_text = None
+    content_json = None
     dir_path = "./test-data"
 
     @classmethod
@@ -22,10 +22,10 @@ class TestJsonReader(unittest.TestCase):
         exp_print = "expected_do.txt"
 
         with open(exp_json, "r") as f:
-            TestJsonReader.word_exp_json = json.load(f)
+            TestJsonReader.content_json = json.load(f)
 
         with open(exp_print, "r") as f:
-            TestJsonReader.word_exp_print = f.read()
+            TestJsonReader.content_text = f.read()
 
         os.makedirs(TestJsonReader.dir_path, exist_ok=True)
 
@@ -34,21 +34,21 @@ class TestJsonReader(unittest.TestCase):
 
     @unittest.skip("SKIP ---- not yet impl")
     def test_jsonToText_returnsAll(self):
-        cmd = JsonReader(TestJsonReader.word_exp_json)
+        cmd = JsonReader(TestJsonReader.content_json)
 
-        text = cmd.read_content(TestJsonReader.word_exp_json)
+        text = cmd.read_content(TestJsonReader.content_json)
 
-        self.assertEqual(text, TestJsonReader.word_exp_print)
+        self.assertEqual(text, TestJsonReader.content_text)
 
     def test_toText_frequency(self):
-        cmd = JsonReader(TestJsonReader.word_exp_json)
+        cmd = JsonReader(TestJsonReader.content_json)
 
         text = cmd.frequency()
 
         self.assertEqual(text, "[Extremely Common]\n\n")
 
     def test_toText_Definitions(self):
-        cmd = JsonReader(TestJsonReader.word_exp_json)
+        cmd = JsonReader(TestJsonReader.content_json)
 
         with patch.object(JsonReader, "_read_gram_groups") as mock_ggroups:
             mock_ggroups.side_effect = ["group1\n", "group2\n", "group3\n", "group4\n", "group5\n"]
