fb28c65316340d66eeefc9040f2b13603f8fe4b4
Samuel Ghinet <fio_244@yahoo.com>
2016-07-04 14:41:26 +0300

    very small refactor

--- v2/src/input_processor.py
+++ v2/src/input_processor.py
@@ -15,25 +15,25 @@ def print_syn(word: str):
     printer.print_syn(content)
 
 
+def get_word():
+    content_retrieval = ContentRetrieval(from_web=False)
+    def_content, learn_content = content_retrieval.get_def_content()
+    syn_content = content_retrieval.get_syn_content()
+    saver = JsonSaver()
+
+    saver.save("do.def", def_content)
+    saver.save("do.learn", learn_content)
+    saver.save("do.syn", syn_content)
+
+    printer = JsonPrinter()
+    printer.print(def_content)
+    printer.print_learn(learn_content)
+
+
 def process_input(input: str):
     matcher = re.match('syn\((.*)\)', input)
     if matcher:
         word = matcher.groups()[0]
         print_syn(word)
     else:
-        content_retrieval = ContentRetrieval(from_web=False)
-
-        def_content, learn_content = content_retrieval.get_def_content()
-        syn_content = content_retrieval.get_syn_content()
-
-        saver = JsonSaver()
-        saver.save("do.def", def_content)
-        saver.save("do.learn", learn_content)
-        saver.save("do.syn", syn_content)
-
-        printer = JsonPrinter()
-        printer.print(def_content)
-
-        printer.print_learn(learn_content)
-
-
+        get_word()
