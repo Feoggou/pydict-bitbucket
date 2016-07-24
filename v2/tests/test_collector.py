714437c3561310990d6b25c5bcd15a8cad238ffb
Samuel Ghinet <fio_244@yahoo.com>
2016-07-24 21:08:57 +0300

    add test_collector. empty atm.

---
+++ v2/tests/test_collector.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest import mock
+from unittest.mock import patch
+
+from src.json_collector import JsonCollector
+
+
+class TestJsonCollector(unittest.TestCase):
+    def test_dummy(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
