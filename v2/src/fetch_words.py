5681aef6f6b0695440def3bea87c7c0459712a82
Samuel Ghinet <fio_244@yahoo.com>
2016-08-07 18:25:41 +0300

    update fetch words

--- v2/src/fetch_words.py
+++ v2/src/fetch_words.py
@@ -12,7 +12,8 @@ sys.path.append("/home/zenith/PycharmProjects/EDictionary/v2")
 
 from src import config
 
-HTML_ALLWORDS_PATH = "/home/zenith/PycharmProjects/EDictionary/v2/html_permanent/all_words.json"
+HTML_ALLWORDS_PATH = "/home/zenith/PycharmProjects/EDictionary/v2/html_permanent/a.json"  # TODO
+HTML_PERMANENT_PATH = "/home/zenith/PycharmProjects/EDictionary/v2/html_permanent/html"
 WORDS_RETRIEVED = 0
 ALL_ITEMS = []
 
@@ -44,14 +45,35 @@ def get_random_item(to_download: list):
     return index
 
 
-def download_word(item: dict):
+def download_html(word: str, http_path, file_suffix: str):
     conn = http.client.HTTPConnection(config.HOSTNAME)
-    print('getting item: "{}" ({})'.format(item["title"], item["link"]))
-    # conn.request("GET", item["link"])
-    # reason = conn.getresponse()
 
-    # data = reason.read()
-    # html_content = data.decode()
+    conn.request("GET", http_path + word)
+    reason = conn.getresponse()
+
+    data = reason.read()
+    html_content = data.decode()
+
+    if len(html_content) == 0:
+        print("{} --- could not be downloaded.".format(word + file_suffix))
+        return False
+
+    file_name = os.path.join(HTML_PERMANENT_PATH, word + file_suffix)
+
+    with open(file_name, "w") as f:
+        f.write(html_content)
+
+    return True
+
+
+def download_word(item: dict):
+    print('downloading... "{}" ({})'.format(item["title"], item["link"]))
+    word = item["link"].split('/')[-1]
+
+    ret = download_html(word, config.HTTP_PATH, "_defs.html")
+    download_html(word, config.SYN_HTTP_PATH, "_syn.html")
+
+    return ret
 
 
 def save_json():
@@ -68,16 +90,15 @@ def timer_callback():
     print("TODO: {} / {} ".format(len(to_download), len(ALL_ITEMS)))
 
     index = get_random_item(to_download)
-    download_word(to_download[index])
+    if download_word(to_download[index]) is True:
+        # NOTE: also writes in all_items
+        to_download[index]["have"] = True
+        WORDS_RETRIEVED += 1
 
-    # NOTE: also writes in all_items
-    to_download[index]["have"] = True
-    WORDS_RETRIEVED += 1
-
-    if WORDS_RETRIEVED > 10:
-        # save_json()
-        WORDS_RETRIEVED = 0
-        exit(0)
+        if WORDS_RETRIEVED >= 10:
+            save_json()
+            WORDS_RETRIEVED = 0
+            exit(0)
 
     # timer = Timer(5, timer_callback)
     # timer.start()
