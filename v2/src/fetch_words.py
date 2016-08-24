64174974ff1224871936f6a747b90b19c84784d2
Samuel Ghinet <fio_244@yahoo.com>
2016-08-24 21:36:57 +0300

    letter: d; switch to s

--- v2/src/fetch_words.py
+++ v2/src/fetch_words.py
@@ -12,8 +12,8 @@ sys.path.append("/home/zenith/PycharmProjects/EDictionary/v2")
 
 from src import config
 
-# a, b, c
-LETTER = "d"
+# a, b, c, d
+LETTER = "s"
 HTML_ALLWORDS_PATH = "/home/zenith/PycharmProjects/EDictionary/v2/html_permanent/{}.json".format(LETTER)  # TODO b, ...
 HTML_PERMANENT_PATH = "/home/zenith/PycharmProjects/EDictionary/v2/html_permanent/html"
 WORDS_RETRIEVED = 0
