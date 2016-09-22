347e60284ab5fe25e842ba2ea2564c8c72f654e5
Samuel Ghinet <samuel.ghinet@gmail.com>
2016-09-22 22:20:37 +0300

    --ammend

--- v2/src/fetch_words.py
+++ v2/src/fetch_words.py
@@ -12,8 +12,8 @@ sys.path.append("/home/zenith/PycharmProjects/EDictionary/v2")
 
 from src import config
 
-# a, b, c, d, p, m, t, l, f
-LETTER = "r"
+# a, b, c, d, p, m, t, l, f, r
+LETTER = "g"
 
 HTML_ALLWORDS_PATH = "/home/zenith/PycharmProjects/EDictionary/v2/html_permanent/{}.json".format(LETTER)
 HTML_PERMANENT_PATH = "/home/zenith/PycharmProjects/EDictionary/v2/html_permanent/html"
