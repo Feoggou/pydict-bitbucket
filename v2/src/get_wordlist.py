dc936c927968388d1a7449cff620041b57a41114
Feoggou <fio_244@yahoo.com>
2016-10-06 23:50:05 +0300

    update get_wordlist.py for updating

--- v2/src/get_wordlist.py
+++ v2/src/get_wordlist.py
@@ -4,11 +4,13 @@ import sys
 
 import http
 import string
+import time
 import json
+import os
 from http import client
 from lxml import etree
 
-sys.path.append("/home/zenith/PycharmProjects/EDictionary/v2")
+sys.path.append("/home/zenith/PycharmProjects/py-dict/v2")
 
 from src import config
 
@@ -16,15 +18,30 @@ conn = http.client.HTTPConnection(config.HOSTNAME)
 base_http_path = "/browse/english/words-starting-with-"
 
 ALL_JSON_ITEMS = []
-HTML_ALLWORDS_PATH = "/home/zenith/PycharmProjects/EDictionary/v2/html_permanent/all_words.json"
+HTML_BASE = "/home/zenith/PycharmProjects/py-dict/v2/html_permanent"
 DOTS = int(0)
+
+
+def HTML_PLUS(letter: str):
+    return os.path.join(HTML_BASE, letter + "-plus.json")
+
+
+def HTML_MINUS(letter: str):
+    return os.path.join(HTML_BASE, letter + "-minus.json")
 
 
 def get_sublist(sublist: etree._Element):
     global DOTS
     http_path = sublist.get("href")
 
-    conn.request("GET", http_path)
+    while True:
+        try:
+            conn.request("GET", http_path)
+            break
+        except:
+            print("sublist error...")
+            time.sleep(20)
+
     reason = conn.getresponse()
 
     data = reason.read()
@@ -37,18 +54,31 @@ def get_sublist(sublist: etree._Element):
     print("{} items{}".format(len(items), DOTS * "."))
     DOTS = (DOTS + 1) % 4
 
+    result_items = []
+
     for item in items:
         title = item.get("title")
         link = item.get("href")
         json_obj = {"title": title, "link": link, "have": False}
-        ALL_JSON_ITEMS.append(json_obj)
+        result_items.append(json_obj)
+        # break  # TODO
 
+    return result_items
 
-for letter in string.ascii_lowercase:
+ALL_LETTERS = 'wxyz'
+
+for letter in ALL_LETTERS:
     http_path = base_http_path + letter
     print("LETTER: {}\n".format(letter))
 
-    conn.request("GET", http_path)
+    while True:
+        try:
+            conn.request("GET", http_path)
+            break
+        except:
+            print("letter error...")
+            time.sleep(20)
+
     reason = conn.getresponse()
 
     data = reason.read()
@@ -60,10 +90,34 @@ for letter in string.ascii_lowercase:
 
     print("Found {} lists or letter '{}'!".format(len(sublists), letter))
 
+    letter_items = []
     for sublist in sublists:
-        get_sublist(sublist)
+        items = get_sublist(sublist)
+        letter_items += items
+
+        # break  # TODO
+
+    existing_file = os.path.join(HTML_BASE, letter + ".json")
+
+    with open(existing_file, "r", encoding="utf-8") as f:
+        existing_items = json.load(f)
+
+    existing_links = [x["link"] for x in existing_items]
+    now_links = [x["link"] for x in letter_items]
+
+    plus_links = set(now_links) - set(existing_links)
+    minus_links = set(existing_links) - set(now_links)
+
+    print("PLUS ITEMS: {}; MINUS ITEMS: {} [existing={}; now={}]".format(len(plus_links), len(minus_links),
+          len(existing_items), len(letter_items)))
+
+    plus_items = [x for x in letter_items if x["link"] in plus_links]
+    minus_items = [x for x in existing_items if x["link"] in minus_links]
+
+    with open(HTML_PLUS(letter), "w", encoding="utf-8") as f:
+        json.dump(plus_items, f, indent=4, sort_keys=True, ensure_ascii=False)
+
+    with open(HTML_MINUS(letter), "w", encoding="utf-8") as f:
+        json.dump(minus_items, f, indent=4, sort_keys=True, ensure_ascii=False)
 
     print("\n\n")
-
-with open(HTML_ALLWORDS_PATH, "w", encoding="utf-8") as f:
-    json.dump(ALL_JSON_ITEMS, f, indent=4, sort_keys=True, ensure_ascii=False)
